65c25805552f2fba7e13ed8ba8655c391619666e
mohansai <mohansai75872@gmail.com>
2024-12-02 16:29:59 +0530

    hello

---
+++ Demo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+class Demo
+{
+public static void main(String []args)
+{
+ArrayList a1=new ArrayList();
+a1.add(100);
+a1.add(123.123);
+a1.add("sai");
+a1.add(null);
+a1.add(100);
+System.out.println(a1); 
+}
+}
